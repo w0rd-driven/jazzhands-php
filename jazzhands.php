ee17a8f77b83b167f888a5e7a99daf01b30c0341
Jeremy Brayton <jeremy@braytonium.com>
2016-01-10 17:33:04 -0500

    Using the Node.js project to guide PHP. This was almost cheating to copy/paste but the syntax is so similar why not? Next up is writing to the output file.

---
+++ jazzhands.php
@@ -0,0 +1,36 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: jbrayton
+     * Date: 1/10/16
+     * Time: 5:12 PM
+     */
+    $config = ["buildDirectory" => "../..", "outputDirectory" => "/build/php/", "outputFile" => "output.txt"];
+
+    function jazzercise($path = null) {
+        echo "Jazzercising...\n";
+        for ($index = 1; $index <= 100; $index++) {
+            $data = $index;
+
+            if ($index % 3 === 0)
+                $data = "Jazz";
+            if ($index % 5 === 0)
+                $data = "Hands";
+            if ($index % 15 === 0)
+                $data = "JazzHands";
+
+            echo "$data\n";
+        }
+    }
+
+    $directory = $config["buildDirectory"].$config["outputDirectory"];
+    $file = $directory.$config["outputFile"];
+    echo "Output directory: $directory\n";
+    echo "Output file: $file\n";
+
+    if (!file_exists($directory)) {
+        mkdir($directory, 0777, true);
+    }
+
+    jazzercise($file);
+?>
